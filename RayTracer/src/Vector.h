047085b5bc9c48769290019c5ba24a5b2b66afdf
Tmrcdsk <129770406+Tmrcdsk@users.noreply.github.com>
2025-02-23 17:04:13 +0800

    简单渲染一个球体

--- RayTracer/src/Vector.h
+++ RayTracer/src/Vector.h
@@ -10,4 +10,24 @@ struct vec3
 
 	float& operator[](int index) { return index == 0 ? x : (index == 1 ? y : z); }
 	const float& operator[](int index) const { return index == 0 ? x : (index == 1 ? y : z); }
+
+	vec3 operator+(const vec3& other) const { return vec3(x + other.x, y + other.y, z + other.z); }
+	vec3 operator-(const vec3& other) const { return vec3(x - other.x, y - other.y, z - other.z); }
+	vec3 operator-() const { return vec3(-x, -y, -z); }
+	vec3 operator*(const vec3& other) const { return vec3(x * other.x, y * other.y, z * other.z); }
+	vec3 operator*(float k) const { return vec3(k * x, k * y, k * z); } // ÓÒ³Ë
+	friend vec3 operator*(float k, const vec3& v); // ×ó³Ë
+
+	float norm() const { return sqrt(x * x + y * y + z * z); }
+	float norm2() const { return x * x + y * y + z * z; }
+	vec3 normalized() const
+	{
+		float length = norm();
+		return vec3(x / length, y / length, z / length);
+	}
 };
+
+vec3 operator*(float k, const vec3& v) { return vec3(k * v.x, k * v.y, k * v.z); }
+
+float dot(const vec3& a, const vec3& b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+vec3 cross(const vec3& a, const vec3& b) { return vec3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x); }
--- RayTracer/src/main.cpp
+++ RayTracer/src/main.cpp
@@ -1,22 +1,74 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 #include "Vector.h"
 
-void render()
+struct Ray
+{
+	vec3 orig;
+	vec3 dir;
+
+	Ray() : orig(0.0f), dir(0.0f) {}
+	Ray(const vec3& o, const vec3& d) : orig(o), dir(d) {}
+
+	vec3 at(float t) const { return orig + t * dir; }
+};
+
+struct Sphere
+{
+	vec3 center;
+	float radius;
+
+	Sphere() : center(0.0f), radius(0.0f) {}
+	Sphere(const vec3& c, float r) : center(c), radius(r) {}
+
+	bool hit(const Ray& ray, float& t0) const {
+		vec3 L = center - ray.orig;
+
+		float a = dot(ray.dir, ray.dir);
+		float b = -2.0f * dot(ray.dir, L);
+		float c = dot(L, L) - radius * radius;
+
+		float discriminant = b * b - 4 * a * c;
+		if (discriminant < 0)
+			return false;
+
+		t0 = (-b - std::sqrt(discriminant)) / (2.0f * a);
+		float t1 = (-b + std::sqrt(discriminant)) / (2.0f * a);
+		if (t0 < 0) t0 = t1;
+		if (t0 < 0) return false;
+		return true;
+	}
+};
+
+vec3 castRay(const Ray& ray, const Sphere& sphere)
+{
+	float sphere_dist = std::numeric_limits<float>::max();
+	// background color
+	if (!sphere.hit(ray, sphere_dist))
+		return vec3(0.2f, 0.3f, 0.8f);
+	// sphere color
+	return vec3(0.8f, 0.2f, 0.3f);
+}
+
+void render(const Sphere& sphere)
 {
 	const int width = 1280;
 	const int height = 720;
-	
+
 	std::vector<vec3> framebuffer(width * height);
 
 	for (uint32_t j = 0; j < height; ++j) {
 		for (uint32_t i = 0; i < width; ++i) {
-			framebuffer[i + j * width] = vec3(i / (float)width, j / (float)height, 0.0f);
+			float x = (2 * (i + 0.5f) / (float)width - 1.0f);
+			float y = -(2 * (j + 0.5f) / (float)height - 1.0f);
+			vec3 dir = vec3(x, y, -1).normalized();
+			framebuffer[i + j * width] = castRay(Ray(vec3(0.0f), dir), sphere);
 		}
 	}
-	
+
 	std::ofstream ofs;
 	ofs.open("out.ppm", std::ios::binary);
 	ofs << "P6\n" << width << " " << height << "\n255\n";
@@ -30,6 +82,7 @@ void render()
 
 int main()
 {
-	render();
+	Sphere sphere(vec3(-3, 0, -16), 2);
+	render(sphere);
 	return 0;
 }
